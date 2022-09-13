b27ab7278fc692e1c9f2e866a74ad2b025879123
SO <78200285+sora3765@users.noreply.github.com>
2022-09-13 14:23:21 +0900

    react-router前
    react-router前

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react'
+import { render } from 'react-dom'
+import styled from 'styled-components'
+import { createGlobalStyle } from 'styled-components'
+import { Editor } from './pages/editor'
+
+const GlobalStyle = createGlobalStyle`
+body * {
+  box-sizing: border-box;
+}
+`
+
+const Main = (
+<>
+  <GlobalStyle />
+  <Editor />
+</>
+)
+
+render(Main, document.getElementById('app'))
---
+++ src/pages/editor.tsx
@@ -0,0 +1,99 @@
+import * as React from 'react'
+import styled from 'styled-components'
+import { useStateWithStorage } from '../hooks/use_state_with_storage'
+import * as ReactMarkdown from 'react-markdown'
+import { putMemo } from '../indexeddb/memos'
+import { Button } from '../components/button'  
+import { SaveModal } from '../components/save_modal'
+
+const { useState } = React
+
+
+const Header = styled.header`
+  align-content: center;
+  display: flex;
+  font-size: 1.5rem;
+  height: 2rem;
+  justify-content: space-between;
+  left: 0;
+  line-height: 2rem;
+  padding: 0.5rem 1rem;
+  position: fixed;
+  right: 0;
+  top: 0;
+`
+
+const Wrapper = styled.div`
+  bottom: 0;
+  left: 0;
+  position: fixed;
+  right: 0;
+  top: 3rem;
+`
+
+const TextArea = styled.textarea`
+  border-right: 1px solid silver;
+  border-top: 1px solid silver;
+  bottom: 0;
+  font-size: 1rem;
+  left: 0;
+  padding: 0.5rem;
+  position: absolute;
+  top: 0;
+  width: 50vw;
+`
+
+const Preview = styled.div`
+  border-top: 1px solid silver;
+  bottom: 0;
+  overflow-y: scroll;
+  padding: 1rem;
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: 50vw;
+`
+const HeaderControl = styled.div`
+    height: 2rem;
+    display: flex;
+    align-content: center;
+  `
+
+const StorageKey = 'pages/editor:text'
+
+export const Editor: React.FC = () => {
+    const [text, setText] = useStateWithStorage('', StorageKey)
+
+    const [showModal, setShowModal] = useState(false)
+
+  return (
+    <>
+      <Header>
+        Markdown Editor
+        <HeaderControl>
+            <Button onClick={() => setShowModal(true)}>
+              保存する
+            </Button>
+        </HeaderControl>
+      </Header>
+      <Wrapper>
+      <TextArea
+            onChange={(event) => setText(event.target.value)}
+            value={text}
+          />
+        <Preview>
+            <ReactMarkdown>{text}</ReactMarkdown>
+        </Preview>
+      </Wrapper>
+      {showModal && (
+          <SaveModal
+            onSave={(title: string): void => {
+              putMemo(title, text)
+              setShowModal(false)
+            }}
+            onCancel={() => setShowModal(false)}
+          />
+        )}
+    </>
+  )
+}
